ef87f2d984ce21a07e8d5cd30add4ee1f336b5cd
Deepak <deepakgangwar4265@gmail.com>
2023-02-06 10:47:03 +0530

    Add 11ty config file, change date to more readable format using luxon filter

---
+++ .eleventy.js
@@ -0,0 +1,16 @@
+const { DateTime } = require('luxon');
+
+module.exports = function (eleventyConfig) {
+  // Copy the `css` directory to the output
+  eleventyConfig.addPassthroughCopy('css');
+
+  // Watch the `css` directory for changes
+  eleventyConfig.addWatchTarget('css');
+
+  // lines describe how to add a custom filter to Eleventy
+  eleventyConfig.addFilter('readableDate', (dateObj) => {
+    return DateTime.fromJSDate(dateObj, { zone: 'utc' }).toFormat(
+      'dd LLL yyyy'
+    );
+  });
+};
