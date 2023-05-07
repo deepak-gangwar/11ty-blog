fb8ba5ffcefec53adb1eecf0a4788dedbb5536ca
Deepak <deepakgangwar4265@gmail.com>
2023-05-07 19:20:35 +0530

    feat: implement rss feed

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ const { DateTime } = require('luxon');
 const blocksToHtml = require('@sanity/block-content-to-html');
 const htmlmin = require('html-minifier');
 const syntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight');
+const rssPlugin = require('@11ty/eleventy-plugin-rss');
 
 module.exports = function (eleventyConfig) {
   // Fucking ignore the node modules inside sanity
@@ -24,6 +25,7 @@ module.exports = function (eleventyConfig) {
 
   // Prism.js plugin for syntax highlighting in coder's block
   eleventyConfig.addPlugin(syntaxHighlight);
+  eleventyConfig.addPlugin(rssPlugin);
 
   // To minify HTML
   eleventyConfig.addTransform('htmlmin', function (content, outputPath) {
@@ -54,6 +56,32 @@ module.exports = function (eleventyConfig) {
       blocks: value,
     });
   });
+
+  // RFC 822 Date for rss feed
+  eleventyConfig.addFilter('buildRFC822Date', function (dateString) {
+    // add a leading 0 to a number if it is only one digit
+    function addLeadingZero(num) {
+      num = num.toString();
+      while (num.length < 2) num = "0" + num;
+      return num;
+    }
+
+    const dayStrings = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+    const monthStrings = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+
+    const timeStamp = Date.parse(dateString);
+    const date = new Date(timeStamp);
+
+    const day = dayStrings[date.getDay()];
+    const dayNumber = addLeadingZero(date.getDate());
+    const month = monthStrings[date.getMonth()];
+    const year = date.getFullYear();
+    const time = `${addLeadingZero(date.getHours())}:${addLeadingZero(date.getMinutes())}:00`;
+    const timezone = date.getTimezoneOffset() === 0 ? "GMT" : "BST";
+
+    //Wed, 02 Oct 2002 13:00:00 GMT
+    return `${day}, ${dayNumber} ${month} ${year} ${time} ${timezone}`;
+  })
 
   // Set up input and output folders
   return {
